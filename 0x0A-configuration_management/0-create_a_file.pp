c13c125b2c7e4b9d7dbc1c867e42803d88ea76f7
sunnywizy <sundayobasi222@gmail.com>
2024-03-23 04:45:47 +0100

    Just created some files to my repository

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # Create a file in /tmp
 
-file { '/tmp/school':
-  ensure  => file,
+file { 'school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
+  path    => '/tmp/school',
 }
